5804dca3a02b57359c56763c9c976522397317cb
Mattia <mattia.malica@gmail.com>
2021-08-13 16:33:40 +0200

    Update week2

--- 1 Month Preparation Kit/Week2/MaxMin/Program.cs
+++ 1 Month Preparation Kit/Week2/MaxMin/Program.cs
@@ -45,41 +45,15 @@ namespace MaxMin
             }
 
             arr.Sort();
-            var unfairnessSum = 0;
-            var sumList = new List<int>();
-            var val = 1 - k;
 
-            sumList.Add(arr[0]);
-            for (int i = 1; i < arr.Count; i++)
+            var min = int.MaxValue;
+
+            for (var i = 0; i < arr.Count - k + 1; i++)
             {
-                sumList.Add(sumList[i - 1] + arr[i]);
+                min = Math.Min(min, arr[i + k - 1] - arr[i]);
             }
 
-            for (int i = 0; i < k; i++)
-            {
-                unfairnessSum += val * arr[i];
-                val += 2;
-            }
-
-            var finalUnfainerssSum = unfairnessSum;
-            for (int i = k; i < arr.Count; i++)
-            {
-                var newUnfairnessSum = unfairnessSum + (k - 1) * arr[i] + (k - 1) * arr[i - k] - 2 * (sumList[i - 1] - sumList[i - k]);
-                finalUnfainerssSum = Math.Min(newUnfairnessSum, finalUnfainerssSum);
-                unfairnessSum = newUnfairnessSum;
-            }
-
-            //for (int i = 0; i < arr.Count - k + 1; i++)
-            //{
-            //    var subArray = arr.GetRange(i, k);
-            //    var currentUnfairnessSum = subArray.Max() - subArray.Min();
-            //    if (unfairnessSum.Equals(-1) || currentUnfairnessSum < unfairnessSum)
-            //    {
-            //        unfairnessSum = currentUnfairnessSum;
-            //    }
-            //}
-
-            return finalUnfainerssSum;
+            return min;
         }
     }
 }
